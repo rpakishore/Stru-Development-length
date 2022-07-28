421f1deb0967801e4e215ef172f18eedbe91ee3d
rpakishore <rpakishore@gmail.com>
2022-07-27 21:36:28 -0700

    Added Upload/Download variables

--- pages/1_Hooked.py
+++ pages/1_Hooked.py
@@ -11,13 +11,13 @@ import json
 input = {
     'fc': 25,
     'fy': 400,
-    'bar': 1,
+    'bar': '15M',
     'side_cover':False,
     'tail_cover': True,
     '3_stirrups': True,
     'epoxy_bars': False,
     'Normal_density': True,
-    'Hook': 0
+    'Hook': '90°'
 }
 
 
@@ -28,40 +28,6 @@ bar_df = pd.DataFrame({
     'hook dia': [60, 90, 100, 150, 200, 250, 400, 550]
     })
 
-# <!-----Heading------>
-st.header("Development of standard hooks in tension | Cl. 12.5")
-# <!-----Inputs------>
-st.subheader("Inputs")
-
-left_column, right_column = st.columns(2)
-
-for key in input.keys():
-    if not key in st.session_state:
-        st.session_state[key] = input[key]
-
-with left_column:
-    input['fc'] = st.slider(
-        label="Concrete compressive Strength (f'c)", min_value=5, max_value=60, value=input['fc'],
-        step=5, help="Clause 12.1.2 limits the max. concrete strength to 64MPa",
-        key='fc')
-    st.write(st.session_state['fc'])
-    input['fy'] = st.slider(label="Steel tensile Strength (fy)", min_value=300, max_value=500, value=input['fy'], step=50)
-    st.write(st.session_state['fy'])
-    options = list(bar_df['bars'])
-    input['bar'] = st.selectbox('Bar size',options,index=input['bar'],)
-    input['bar'] = options.index(input['bar'])
-
-    options = ('180°', '90°')
-    input['Hook'] = st.selectbox('Hook type',options, index=input['Hook'], key='Hook')
-    input['Hook'] = options.index(input['Hook'])
-
-with right_column:
-    st.write("**Options**")
-    input['side_cover'] = st.checkbox("Side cover < 60mm", input['side_cover'], help="side cover (normal to plane of hook) is less than 60mm", key='side_cover')
-    input['tail_cover'] = st.checkbox("Tail cover > 50mm", input['tail_cover'], help="For 90° hooks, cover on bar extension beyond the hook is not less than 50mm", key='tail_cover')
-    input['3_stirrups'] = st.checkbox("Atleast 3 stirrups or ties", input['3_stirrups'], help=f"Hook is enclosed vertically/horizontally within 3 ties/stirrups spaced along a length of > {int(bar_df['hook dia'][input['bar']])} mm and a spacing <= {round(bar_df['size'][input['bar']]*3,1)} mm", key='3_stirrups')
-    input['epoxy_bars'] = st.checkbox("Epoxy coated bars", input['epoxy_bars'], key='epoxy_bars')
-    input['Normal_density'] = st.checkbox("Normal-density concrete", input['Normal_density'], key='Normal_density')
 
 # <!-----Functions------>
 
@@ -110,9 +76,9 @@ def k1(input):
         float,str: returns the k1 parameter and comment justification
     """
     k_1 = 1.0
-    if input['bar'] < 6:
+    if input['bar'] != "45M" and input['bar'] != "55M":
         if not input['side_cover']:
-            if input['Hook'] == 1:
+            if input['Hook'] == '90°':
                 if input['tail_cover']:
                     k_1 = 0.7
                     comment = "Side cover > 60mm\ |\ 90°\ hook\ |\ Tail\ cover > 50mm"
@@ -138,7 +104,7 @@ def k2(input):
         float,str: returns the k2 parameter and comment justification
     """
     k_2 = 1.0
-    if input['bar'] < 6:
+    if input['bar'] != "45M" and input['bar'] != "55M":
         if input['3_stirrups']:
             k_2 = 0.8
             comment = "Ties/Stirrup\ requirement\ met"
@@ -151,9 +117,90 @@ def k2(input):
 
 def update_inputs():
     for key in input.keys():
-        st.session_state[key] = new_input[key]
+        st.session_state[key] = input[key]
     return
+# <!-----Heading------>
+st.header("Development of standard hooks in tension | Cl. 12.5")
+# <!-----Inputs------>
+st.subheader("Inputs")
 
+left_column, right_column = st.columns(2)
+
+for key in input.keys():
+    if not key in st.session_state:
+        st.session_state[key] = input[key]
+
+with left_column:
+    input['fc'] = st.slider(
+        label="Concrete compressive Strength (f'c)", 
+        min_value=5, 
+        max_value=60, 
+        value=input['fc'],
+        step=5, 
+        help="Clause 12.1.2 limits the max. concrete strength to 64MPa",
+        key='fc')
+
+    input['fy'] = st.slider(
+        label="Steel tensile Strength (fy)", 
+        min_value=300, 
+        max_value=500, 
+        value=input['fy'], 
+        step=50, 
+        key='fy')
+
+    options = tuple(bar_df['bars'])
+    input['bar'] = st.selectbox(label='Bar size',
+                                options=options,
+                                index=options.index(input['bar']),
+                                key='bar')
+    if type(input['bar']) == int:
+        input['bar'] = options[input['bar']]
+
+    options = ('180°', '90°')
+    input['Hook'] = st.selectbox(label='Hook type',
+                                 options=options, 
+                                 index=options.index(input['Hook']), 
+                                 key='Hook')
+    if type(input['Hook']) == int:
+        input['Hook'] = options[input['Hook']]
+
+with right_column:
+    st.write("**Options**")
+    input['side_cover'] = st.checkbox(label="Side cover < 60mm",
+                                      value=input['side_cover'], 
+                                      help="side cover (normal to plane of hook) is less than 60mm", 
+                                      key='side_cover')
+    input['tail_cover'] = st.checkbox(label="Tail cover > 50mm", 
+                                      value=input['tail_cover'], 
+                                      help="For 90° hooks, cover on bar extension beyond the hook is not less than 50mm", 
+                                      key='tail_cover')
+    input['3_stirrups'] = st.checkbox(label="Atleast 3 stirrups or ties", 
+                                      value=input['3_stirrups'], 
+                                      help=f"Hook is enclosed vertically/horizontally within 3 ties/stirrups spaced along a length of > {int(bar_df[bar_df['bars']==input['bar']]['hook dia'].iloc[0])} mm and a spacing <= {round(bar_df[bar_df['bars']==input['bar']]['size'].iloc[0]*3,1)} mm", 
+                                      key='3_stirrups')
+    input['epoxy_bars'] = st.checkbox(label="Epoxy coated bars", 
+                                      value=input['epoxy_bars'], 
+                                      key='epoxy_bars')
+    input['Normal_density'] = st.checkbox(label="Normal-density concrete", 
+                                          value=input['Normal_density'], 
+                                          key='Normal_density')
+    
+    uploaded_file = st.file_uploader(
+        label="Upload variables",
+        type = "json",
+        help="Import previously used variables",
+        accept_multiple_files=False
+    )
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        input = json.load(uploaded_file)
+
+        st.button(
+            label="Update",
+            on_click=update_inputs,
+            key="update_button"
+            )
+        
 # <!-----Calculations------>
 k_1, k_1_comment = k1(input)
 k_2, k_2_comment = k2(input)
@@ -178,11 +225,11 @@ calculation_latex, calculation_value = calculation_steps(
     k_2=k_2,
     k_3=k_3,
     k_4=k_4,
-    d_b=bar_df['size'][input['bar']],
+    d_b=bar_df[bar_df['bars']==input['bar']]['size'].iloc[0],
     f_prime_c=input['fc'],
     f_y = input['fy']
     )
-result_latex, result_value = development_length(calculation_value['l_hb'], bar_df['size'][input['bar']])
+result_latex, result_value = development_length(calculation_value['l_hb'], bar_df[bar_df['bars']==input['bar']]['size'].iloc[0])
 st.latex(result_latex + '\ mm\ |\ (Cl. 12.5.1)')
 st.latex("l_{dh} = " + str(int(result_value['l_dh'])) + '\ mm')
 
@@ -194,24 +241,9 @@ st.latex("k_{3} =" + str(k_3) +"\ (" + k_3_comment + ")")
 st.latex("k_{4} =" + str(k_4) +"\ (" + k_4_comment + ")")
 st.latex(calculation_latex)
 
-st.write(st.session_state)
+#st.write(st.session_state)
 _, middle_column,_ = st.columns(3)
-with middle_column:
-    uploaded_file = st.file_uploader(
-        label="Upload variables",
-        type = "json",
-        help="Import previously used variables",
-        accept_multiple_files=False
-    )
-    if uploaded_file is not None:
-        bytes_data = uploaded_file.getvalue()
-        new_input = json.load(uploaded_file)
 
-
-    st.button(
-        label="Update",
-        on_click=update_inputs
-        )
 
 with middle_column:
     st.download_button(
