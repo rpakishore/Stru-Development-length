c461c7a275fd05aa80e582572b65d3579ae25230
rpakishore <rpakishore@gmail.com>
2022-08-13 11:48:10 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
-:: cd C:\Users\rpaki\Documents\GitHub\Template-Python
-cd /D "%~dp0"
+cd C:\Users\rpaki\Documents\GitHub\Template-Python
 @echo Started: %date% %time%
-"venv\Scripts\activate.bat" && python -m streamlit run About.py
+"venv\Scripts\activate.bat" && python -m streamlit run preface.py
